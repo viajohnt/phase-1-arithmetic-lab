f31176659e29aaeec0c59869b7b9f634fb6362f1
John Truong <viajohnt@gmail.com>
2023-03-17 22:21:55 -0600

    Mathing

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 15;
+const num4 = 11;
+const mod = num3 % num4;
+
+const numbers = [1, 3, 5, 10, 20];
+const max = Math.max(...numbers);
